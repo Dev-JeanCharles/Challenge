afb57e9122cd0ad020a31cd837d8e4839fe2e4a8
Dev-JeanCharles <jeancduasan@gmail.com>
2023-12-14 11:56:46 -0300

    Updated

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,10 @@ public class Main {
         String path_noe = "src/arquivo/noe.txt";
         String path_senhor = "src/arquivo/senhor.txt";
 
-       ParteUm.lerArquivo(path_solo);
-       ParteUm.buscarAnimal(path_repeticao);
-       ParteUm.lerArquivo(path_noe);
-       ParteUm.triplicarSolo(path_solo);
-       ParteDois.movimentarCorpo(path_senhor);
+        ParteUm.lerArquivo(path_solo);
+        ParteUm.buscarAnimal(path_repeticao);
+        ParteUm.lerArquivo(path_noe);
+        ParteUm.triplicarSolo(path_solo);
+        ParteDois.movimentarCorpo(path_senhor);
     }
 }
